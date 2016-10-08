67781b25d27cfb486fcb5bb0ad53798e8df0fe0b
Linus Gordon <linusgordon@gmail.com>
2016-10-07 20:40:58 -0400

    debugging heroku H10 error

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
     const bodyParser = require('body-parser')
     const request = require('request')
     const app = express()
-    const token = process.env.FB_PAGE_ACCESS_TOKEN
 
     app.set('port', (process.env.PORT || 5000))
 
@@ -39,21 +38,13 @@
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
-	    if (text === 'Generic') {
-                sendGenericMessage(sender)
-                continue
-            }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
-    }
-    if(event.postback) {
-	let text = JSON.stringify(event.postback)
-	sendTextMessage(sender, "Postback received: " + text.substring(0,200), token);
-	continue;
     }
     res.sendStatus(200)
 	})
 
+    const token = "EAAJcqgImFZAQBAEeiOxBKOWHhOv9ZBV3zuNiWiULEWWVUuS0aJGZBW3gpddCQYerfbjysRNk6VIFcvyoFtuM6bXikwzkJxYgELHfy79eFerwQxhxnEesRVJK9COs6ybfvXmDn1egRn5Q8jVX3pJ0vRJZBTjsRXawXTyETUZBHdAZDZD"
 
     function sendTextMessage(sender, text) {
     let messageData = { text:text }
@@ -73,52 +64,3 @@
 	    }
 	})
 }
-
-    function sendGenericMessage(sender) {
-	let messageData = {
-	    "attachment": {
-		"type": "template",
-		"payload": {
-		    "template_type": "generic",
-		    "elements": [{
-			    "title": "First card",
-			    "subtitle": "Element #1 of an hscroll",
-			    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-			    "buttons": [{
-				    "type": "web_url",
-				    "url": "https://www.messenger.com",
-				    "title": "web url"
-				}, {
-				    "type": "postback",
-				    "title": "Postback",
-				    "payload": "Payload for first element in a generic bubble",
-				}],
-			}, {
-			    "title": "Second card",
-			    "subtitle": "Element #2 of an hscroll",
-			    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-			    "buttons": [{
-				    "type": "postback",
-				    "title": "Postback",
-				    "payload": "Payload for second element in a generic bubble",
-				}],
-			}]
-		}
-	    }
-	}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		    qs: {access_token:token},
-		    method: 'POST',
-		    json: {
-		    recipient: {id:sender},
-			message: messageData,
-			}
-	    }, function(error, response, body) {
-		if (error) {
-		    console.log('Error sending messages: ', error)
-			} else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-			}
-	    })
-	    }
